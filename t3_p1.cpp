5edb4684a17ff68e22662965621fb65cb4168a89
Constantin Brindusoiu <BrandConstantin@users.noreply.github.com>
2024-05-20 20:51:50 +0200

    Hola mundo!
    Hola mundo

---
+++ t3_p1.cpp
@@ -0,0 +1,16 @@
+/* Proyecto 1: entrada/salida de datos */
+
+// importar librería principal de E/S
+#include <iostream>
+
+// Para evitar declaraciones individuales de E/S
+using namespace std;
+
+// Declaramos la función principal
+int main(){
+	// Salida de datos por pantalla
+	cout << "Hola mundo! " << endl;
+	
+	// Buena práctica en funciónes enteras
+	return 0;
+}
