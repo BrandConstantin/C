380be3d96e224b2ff25cd2f02b2a26a687d4257c
Constantin Brindusoiu <BrandConstantin@users.noreply.github.com>
2024-05-22 21:16:38 +0200

    Asignaciones e incrementos
    Asignaciones e incrementos

---
+++ t3_p8.cpp
@@ -0,0 +1,19 @@
+/* Asignaciones e incrementos */
+#include <iostream>
+
+int main(){
+	int x, y;
+	int z, a, b, c;
+	x = 4;
+	y = 7;
+	
+	std::cout << x << ' ' << y << std::endl;
+	x = y + 3;
+	std::cout << x << ' ' << y << std::endl;
+	std::cout << (z - 2) * 10 << std::endl;
+	std::cout << z << std::endl;
+	a = b = c = 6; 
+	std::cout << a << std::endl;
+	
+	return 0;
+}
