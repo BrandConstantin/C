fe4508955cf88979ec570f38f4553926232f779b
Constantin Brindusoiu <BrandConstantin@users.noreply.github.com>
2024-05-22 21:02:01 +0200

    Operadores matemáticos
    Operadores matemáticos

---
+++ t3_p6.cpp
@@ -0,0 +1,11 @@
+/*
+Exresiones aritméticas
+*/
+
+#include<iostream>
+
+int main(){
+	std::cout << 4 + 3 + 2 << ' ' << (4 + 2) + 3 << std::endl;
+	std::cout << 12 / 3 * 2 << ' ' << 12 / (2 * 3) << std::endl;
+	return 0;
+}
