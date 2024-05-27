583218f29403a1fbc7de673b1d28af066b9c53b8
Constantin Brindusoiu <BrandConstantin@users.noreply.github.com>
2024-05-27 20:26:29 +0200

    Sentencia lógica if
    Sentencia lógica if

---
+++ t3_p11.cpp
@@ -0,0 +1,24 @@
+/* Sentencia lógica if */ 
+#include <iostream>
+using namespace std;
+
+int main(){
+	const char VOCAL = 'e';
+	char vocal;
+	bool acertado;
+	
+	do{
+		cout << "Indroduzca una vocal: ";
+		cin >> vocal;
+		
+		if(vocal == VOCAL){
+			acertado = true;
+			cout << "Enhorabuen, has acertado\n";
+		}else{
+			cout << "No es correcto!\n";
+		}
+		
+	}while(!acertado);
+	
+	return 0;
+}
