1b789d86d5b9fbb6add78608fc95ee17b2d23264
Constantin Brindusoiu <BrandConstantin@users.noreply.github.com>
2024-05-29 21:15:14 +0200

    for
    for

---
+++ 17_bucle_for.cpp
@@ -0,0 +1,14 @@
+/* Bucles - for */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	int numero;
+	
+	for(numero = 1; numero <= 10; numero++){
+		cout << numero << " al cuadrado es " << numero * numero << endl;
+	}
+
+	return 0;	
+}
